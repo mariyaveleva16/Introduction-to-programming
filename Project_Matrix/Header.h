2a2fa4fa553873c048448f5530f9a4af944063f8
Mariya Veleva <56577507+mariyaveleva16@users.noreply.github.com>
2021-06-24 22:44:53 +0300

    Create Header.h

---
+++ Project_Matrix/Header.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <windows.h>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+const int r = 25, c = 30;
+
+void goToCordinates(int x, int y);
+void FillArray(char array[][c]);
