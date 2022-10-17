6970e965f093103a3c01885b6bfbb78d6d35aa02
Maria Veleva <56577507+mariyaveleva16@users.noreply.github.com>
2022-10-17 13:30:05 +0300

    Update swap(2).cpp

--- Basic problems/swap(2).cpp
+++ Basic problems/swap(2).cpp
@@ -1,1 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+void swap(int &a, int &b){
+    a=a*b;
+    b=a/b;
+    a=a/b;
+}
+
+int main() {
+    int a = 5, b = 10;
+    cout << a << " " << b<<endl;
+    swap(a,b);
+    cout << a << " " << b<<endl;
+    
+}
